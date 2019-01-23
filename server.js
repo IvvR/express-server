2a839478d2f5b669e22ea9ace0db7c8c59126673
IvvR <iv_rn@protonmail.com>
2019-01-22 17:37:49 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}  ${req.url}`;
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log('Unable to uppemd to server.log' + err.message);
+    }
+  });
+  console.log(log);
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs', {
+//     pageTitle: 'Maintenance',
+//     pageHeader: 'The server is under maintenance. We will be right back'
+//   });
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home page',
+    pageHeader: 'Welcome',
+    welcomeMsg: 'Welcome to IvvR home page'
+  });
+  // res.send('<h1>Hi Ivan, I am Express !</h1>');
+  // res.send({
+  //   name: 'Ivan',
+  //   likes: ['bykes', 'yoga']
+  // });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About page',
+    pageHeader: 'Head news'
+  });
+  // res.send('About IvvR page');
+});
+
+// bad request
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.get('/maintenence', (req, res) => {
+  res.render('maintenence.hbs', {
+    pageTitle: 'Maintenence',
+    pageHeader: 'The server is under maintenence. We will be right back'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
